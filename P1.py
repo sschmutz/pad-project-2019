ded4a76a5958c998d940f9934a7c9736d3513a33
sschmutz <stefan.schmutz@outlook.com>
2019-10-24 22:00:41 +0200

    refactor code based on Pylint

--- P1.py
+++ P1.py
@@ -1,3 +1,5 @@
+"""This module reads labels and sequences from a text file."""
+
 import os
 import sys
 
@@ -14,18 +16,18 @@ def ParseSeqFile(path, filename):
     seq_pairs = []
 
     for line in seq_file:
-        if ValidatedSeq(line) != None:
+        if ValidatedSeq(line) is not None:
             seq_pairs.append(ValidatedSeq(line))
-        
+
     seq_file.close()
-    
-    if len(seq_pairs) != 0:
+
+    if seq_pairs:
         return seq_pairs
-    else:
-        # only returns this if all lines in sequence file are invalid
-        return "malformed input"  
-        
-    
+
+    # only returns this if all lines in sequence file are invalid
+    return "malformed input"
+
+
 
 def OpenSeqFile(path, filename):
     """Opens a sequence file if possible and returns it."""
@@ -36,13 +38,13 @@ def OpenSeqFile(path, filename):
     except:
         sys.exit("Combination of path and filename can't be opened.")
 
-    return(seq_file)
-    
-    
-    
+    return seq_file
+
+
+
 def ValidatedSeq(line):
     """Read line from SeqFile and return matching label and sequence in a tuple.
-    
+
     Checks format of line from sequence file, stips all whitespace characters
     and only returns the label and nucleotide sequence if formatted properly:
         - Line starts with a ">" character
@@ -51,26 +53,26 @@ def ValidatedSeq(line):
     """
 
     if line[0] == ">":
-        
+
         try:
             label = line[1:].split()[0]
-            
-            nucleotides_raw = line[1:].split(maxsplit = 1)[1]
+
+            nucleotides_raw = line[1:].split(maxsplit=1)[1]
             nucleotides = "".join(nucleotides_raw.split())
             #TODO are the next scripts case sensitive?
         except:
             return None
-    
-    valid_nucleotides = "ATGC"
-        
-    for nucleotide in nucleotides:
-        if nucleotide not in valid_nucleotides:
-            return None
 
-    return (label, nucleotides)
+        valid_nucleotides = "ATGC"
+
+        for nucleotide in nucleotides:
+            if nucleotide not in valid_nucleotides:
+                return None
+
+        return (label, nucleotides)
 
 
 
-path = "/Users/stefan_schmutz/Documents/GitHub/pad-project-2019/input"
-filename = "sequeces_example_1.txt"
-print(ParseSeqFile(path, filename))
+PATH = "/Users/stefan_schmutz/Documents/GitHub/pad-project-2019/input"
+FILENAME = "sequeces_example_1.txt"
+print(ParseSeqFile(PATH, FILENAME))

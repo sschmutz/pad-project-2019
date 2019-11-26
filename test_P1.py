05540a68c6a46352640594e6a49335d71b1b908b
sschmutz <stefan.schmutz@outlook.com>
2019-11-26 19:02:47 +0100

    add tests

--- test_P1.py
+++ test_P1.py
@@ -1,9 +1,8 @@
-import os
 import P1
+import pytest
 
 def test_ParseSeqFile():
-    path = "./input/"
-    P1_output = P1.ParseSeqFile(os.path.join(path, "sequeces_example.txt"))
+    P1_output = P1.ParseSeqFile("./input/sequences_example.txt")
     P1_output_expected = [('Mouse', 'ACCAAACATCCAAACACCAACCCCAGCCCTTACGCAATCATACAAAGAATATT'),
                           ('Bovine', 'ACCAAACCTGTCCCCATCTAACACCAACCCACATATACAAGCTAAACCAAAAATACC'),
                           ('Gibbon', 'ACTATACCCACCCAACTCGACCTACACCAATCCCCACATAGCACACAGACCAACAACCTC'),
@@ -13,3 +12,30 @@ def test_ParseSeqFile():
                           ('Human', 'ACCCCACTCACCCATACAAACCAACACCACTCTCCACCTAATATACAAATACCTC')]
 
     assert P1_output == P1_output_expected
+
+def test_Fails():
+    with pytest.raises(Exception, match="malformed input"):
+        P1.ParseSeqFile("./input/sequences_fail1.txt")
+
+    with pytest.raises(Exception, match="malformed input"):
+        P1.ParseSeqFile("./input/sequences_fail2.txt")
+
+    with pytest.raises(Exception, match="malformed input"):
+        P1.ParseSeqFile("./input/sequences_fail3.txt")
+
+    with pytest.raises(Exception, match="malformed input"):
+        P1.ParseSeqFile("./input/sequences_fail4.txt")
+
+    with pytest.raises(Exception, match="malformed input"):
+        P1.ParseSeqFile("./input/sequences_fail5.txt")
+
+def test_SpecialCases():
+    P1_output_1 = P1.ParseSeqFile("./input/sequences_special1.txt")
+    P1_output_expected_1 = [('1', 'ACCCCATTATCCATAAAAACCAACACCAACCCCCATCTAACACACAAACTAATGACCCC')]
+
+    assert P1_output_1 == P1_output_expected_1
+
+    P1_output_2 = P1.ParseSeqFile("./input/sequences_special2.txt")
+    P1_output_expected_2 = [('Gorilla', 'ACCCCATTATCCATAAAAACCAACACCAACCCCCATCTAACACACAAACTAATGACCCC')]
+
+    assert P1_output_2 == P1_output_expected_2

c4c70c9369aafd172f1d98d298018e2d65441577
sschmutz <stefan.schmutz@outlook.com>
2019-11-26 19:06:42 +0100

    add comments to tests

--- test_P1.py
+++ test_P1.py
@@ -14,27 +14,30 @@ def test_ParseSeqFile():
     assert P1_output == P1_output_expected
 
 def test_Fails():
+    # no valid line
     with pytest.raises(Exception, match="malformed input"):
         P1.ParseSeqFile("./input/sequences_fail1.txt")
 
+    # label consists of more than one word
     with pytest.raises(Exception, match="malformed input"):
         P1.ParseSeqFile("./input/sequences_fail2.txt")
 
+    # invalid nucleotide letter
     with pytest.raises(Exception, match="malformed input"):
         P1.ParseSeqFile("./input/sequences_fail3.txt")
 
+    # no nucleotide sequence
     with pytest.raises(Exception, match="malformed input"):
         P1.ParseSeqFile("./input/sequences_fail4.txt")
 
-    with pytest.raises(Exception, match="malformed input"):
-        P1.ParseSeqFile("./input/sequences_fail5.txt")
-
 def test_SpecialCases():
+    # label is an integer
     P1_output_1 = P1.ParseSeqFile("./input/sequences_special1.txt")
     P1_output_expected_1 = [('1', 'ACCCCATTATCCATAAAAACCAACACCAACCCCCATCTAACACACAAACTAATGACCCC')]
 
     assert P1_output_1 == P1_output_expected_1
 
+    # one sequence missing
     P1_output_2 = P1.ParseSeqFile("./input/sequences_special2.txt")
     P1_output_expected_2 = [('Gorilla', 'ACCCCATTATCCATAAAAACCAACACCAACCCCCATCTAACACACAAACTAATGACCCC')]
 
